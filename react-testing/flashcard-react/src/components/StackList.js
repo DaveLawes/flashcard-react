8b0b96dd3555755a78373e8b6ec7290549e7a2dc
David Lawes <davidlawes@hotmail.co.uk>
2018-11-14 11:42:39 +0000

    setup: added react routes and links

--- react-testing/flashcard-react/src/components/StackList.js
+++ react-testing/flashcard-react/src/components/StackList.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import stacks from '../data/stacks.json';
 
 class StackList extends Component {
   render() {
-    return(
+    return (
       <div>
-      {
-        stacks.map(stack => {
-          return (
-            <h4 key={stack.id}>{stack.title}</h4>
-          )
-        })
-      }
+        {
+          stacks.map(stack => {
+            return (
+              <Link to='/stack'>
+                <h4 key={stack.id}>{stack.title}</h4>
+              </Link>
+            )
+          })
+        }
       </div>
     )
   }
