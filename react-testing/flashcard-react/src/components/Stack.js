e47f30a4d685e96502338fede3888b1c9bf3b71a
David Lawes <davidlawes@hotmail.co.uk>
2018-11-16 17:56:11 +0000

    style: added style to Card component

--- react-testing/flashcard-react/src/components/Stack.js
+++ react-testing/flashcard-react/src/components/Stack.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Card from './Card';
 
 class Stack extends Component {
   render() {
     const { stack } = this.props;
-    console.log('stack props', stack)
+
     return (
       <div>
-        <Link to='/'>Home</Link>
+        <Link className='link-home' to='/'>
+          <h4>Home</h4>
+        </Link>
         <h3>{stack.title}</h3>
         {
-          stack.cards.map(card =>
-            <div key={card.id}>
-              {card.prompt}
-            </div>
-          )
+          stack.cards.map(card => {
+            return (
+              <Card key={card.id} card={card}/>
+            )
+          })
         }
       </div>
     )
