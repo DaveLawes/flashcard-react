6eb0bc47fc441adbbbd847477bbcd84d97fd1c7b
David Lawes <davidlawes@hotmail.co.uk>
2018-11-16 17:02:20 +0000

    feature: connected stack component to store

--- react-testing/flashcard-react/src/components/Stack.js
+++ react-testing/flashcard-react/src/components/Stack.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Stack extends Component {
   render() {
+    const { stack } = this.props;
+    console.log('stack props', stack)
     return (
       <div>
         <Link to='/'>Home</Link>
-        <h3>Test</h3>
+        <h3>{stack.title}</h3>
+        {
+          stack.cards.map(card =>
+            <div key={card.id}>
+              {card.prompt}
+            </div>
+          )
+        }
       </div>
     )
   }
 }
 
-export default Stack;
+function mapStateToProps(state) {
+  return { stack: state }
+}
+
+const connectComponent = connect(mapStateToProps, null);
+
+export default connectComponent(Stack);
